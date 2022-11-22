be4c02e6bd5c5dae403db7eae2931060082bb0f1
garhajis <118834668+garhajis@users.noreply.github.com>
2022-11-22 20:35:42 +0300

    Add files via upload

---
+++ GS.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*if else statement*/
+int main()
+{int marks;
+marks=39;
+if(marks>=70)
+printf("GRADE A" );
+else if(marks>=60)
+printf("GRADE B");
+else if(marks>=50)
+printf("GRADE C");
+else if(marks>=40)
+printf("GRADE D");
+else
+printf("F");
+return 0;
+}
